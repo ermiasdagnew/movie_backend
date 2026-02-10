5ca55a626622a1a6f0f99ef79ba95e36ee3b5112
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-02-10 13:33:41 +0300

    Update urls.py

--- movie_backend/urls.py
+++ movie_backend/urls.py
@@ -2,20 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 
-# Simple home view for the root URL
+# Simple home view for '/'
 def home(request):
     return HttpResponse("🎬 Movie Recommendation Backend is live! Visit /api/movies/ to see movies.")
 
 urlpatterns = [
-    # Root path shows a simple message
+    # Root path
     path('', home, name='home'),
 
     # Admin panel
     path('admin/', admin.site.urls),
 
     # Movies API
-    path('api/movies/', include('movies.urls')),
-
-    # Swagger documentation (if you have it set up)
-    path('swagger/', include('swagger.urls')),  # remove if you don't have swagger app
+    path('api/movies/', include('movies.urls')),  # Make sure movies app has urls.py
 ]
